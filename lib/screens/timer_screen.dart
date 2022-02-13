a13c0526b5377e69bbf13c83ea2035cb825b9e9f
Jubayer <62088928+jbjason@users.noreply.github.com>
2022-02-14 00:00:41 +0600

    s

--- lib/screens/timer_screen.dart
+++ lib/screens/timer_screen.dart
@@ -58,35 +58,7 @@ class _TimerScreenState extends State<TimerScreen> {
             MinuteIncreasingContainer(minuteController: _minuteController),
             InkWell(
               onTap: () {},
-              child: Container(
-              //  width: 150,
-                child: const Text('Start',textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 1.4,
-                  ),
-                ),
-                padding: const EdgeInsets.all(30),
-                decoration: BoxDecoration(
-                  color: Colors.grey[300],
-                  borderRadius: BorderRadius.circular(12),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.shade500,
-                      offset: const Offset(4, 4),
-                      blurRadius: 15,
-                      spreadRadius: 8,
-                    ),
-                    const BoxShadow(
-                      color: Colors.white,
-                      offset: Offset(-4, -4),
-                      blurRadius: 15,
-                      spreadRadius: 1,
-                    ),
-                  ],
-                ),
-              ),
+              child: const StartButtonBottom(),
             ),
           ],
         ),
@@ -158,6 +130,44 @@ class _TimerScreenState extends State<TimerScreen> {
             offset: const Offset(4, 4),
             blurRadius: 15,
             spreadRadius: 3,
+          ),
+          const BoxShadow(
+            color: Colors.white,
+            offset: Offset(-4, -4),
+            blurRadius: 15,
+            spreadRadius: 1,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class StartButtonBottom extends StatelessWidget {
+  const StartButtonBottom({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Text('Start',textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 1.4,
+        ),
+      ),
+      padding: const EdgeInsets.all(30),
+      decoration: BoxDecoration(
+        color: Colors.grey[300],
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.shade500,
+            offset: const Offset(4, 4),
+            blurRadius: 15,
+            spreadRadius: 8,
           ),
           const BoxShadow(
             color: Colors.white,
